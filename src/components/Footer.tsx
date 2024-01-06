7e93ea5d2ac6b746b897d55bcba14062e669518e
boyczuk <adlaibridsonboyczuk@gmail.com>
2024-01-06 17:22:08 -0500

    Footer added, need links

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,16 +1,22 @@
 import './Footer.css';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import MailOutlineIcon from '@mui/icons-material/MailOutline';
+import TwitterIcon from '@mui/icons-material/Twitter';
 
 function Footer() {
     return (
         <div className='bar'>
-            <h2>@ Adlai.ca</h2>
-            <h2>Video game data from IDGB Database. (Link site)</h2>
+            <div className='text'>
+                <h2>@ Adlai.ca</h2>
+                <h2>Video game data from IDGB Database. (Link site)</h2>
+            </div>
 
             <div className='social_media-links'>
-                <p>IG</p>
-                <p>FB</p>
-                <p>GM</p>
-                <p>X</p>
+                <InstagramIcon />
+                <FacebookIcon />
+                <MailOutlineIcon />
+                <TwitterIcon />
             </div>
         </div>
     );
