8359b83dc4c1c8165de166fffb140f095cb5b1ac
boyczuk <adlaibridsonboyczuk@gmail.com>
2024-01-06 01:50:38 -0500

    Added fade + fixed positioning

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,4 @@
+import Button from '@mui/material/Button';
 import './Home.css';
 
 const Home = () => {
@@ -5,7 +6,8 @@ const Home = () => {
         <div className="page">
 
             <div className="body">
-                <h2>Track and rank your favourite games</h2>
+                <h2>Track and rank <br></br>your favourite games</h2>
+                <Button className="gs_button" variant="contained">GET STARTED</Button>
             </div>
         </div>
     )
