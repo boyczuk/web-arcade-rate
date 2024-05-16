d4f3ab1fb0fb2a2a3a0c5dbd73bccf3092394d19
adlai <adlaibridsonboyczuk@gmail.com>
2024-05-16 00:08:30 -0400

    POST request to add games

--- server.js
+++ server.js
@@ -3,6 +3,18 @@ const cors = require('cors');
 const { getAccessToken } = require('./auth');
 const { searchGames } = require('./igdb'); 
 
+const bodyParser = require('body-parser');
+const admin = require('firebase-admin');
+
+const serviceAccount = require(path.join(__dirname, 'config/serviceAccountKey.json'));
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    projectId: 'arcade-rate'
+});
+
+const db = admin.firestore()
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -21,7 +33,34 @@ app.get('/search-games', async (req, res) => {
 });
 
 app.post('/add-game', async (req, res) => {
-    
+    const { userId, gameId, gameName } = req.body;
+
+    if (!userId || !gameId) {
+        return res.status(400).send("Missing userId or gameId");
+    }
+
+    try {
+        const userRef = db.collection('users').doc(userId);
+        const userDoc = await userRef.get();
+
+        if (!userDoc.exists) {
+            return res.status(404).send("User not found");
+        }
+
+        const userData = userDoc.data();
+        let updatedGames = userData.games ? userData.games.split(',').map(Number) : [];
+
+        if (!updatedGames.includes(gameId)) {
+            updatedGames.push(gameId);
+            await userRef.update({ games: updatedGames.join(',') });
+            return res.status(200).send("Game added successfully");
+        } else {
+            return res.status(400).send("Game already exists");
+        }
+    } catch (error) {
+        console.error('Error updating user profile:', erorr);
+        return res.status(500).send('Internal server error.');
+    }
 });
 
 app.listen(PORT, () => {
