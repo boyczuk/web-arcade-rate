0a9383e318c26e682206201121ca990b30697de3
adlai <adlaibridsonboyczuk@gmail.com>
2024-09-03 15:50:59 -0400

    Added social media links

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,6 @@
 import './Footer.css';
 import InstagramIcon from '@mui/icons-material/Instagram';
-import FacebookIcon from '@mui/icons-material/Facebook';
-import MailOutlineIcon from '@mui/icons-material/MailOutline';
+import YouTubeIcon from '@mui/icons-material/YouTube';
 import TwitterIcon from '@mui/icons-material/Twitter';
 
 function Footer() {
@@ -9,14 +8,23 @@ function Footer() {
         <div className='bar'>
             <div className='text'>
                 <h2>@ Adlai.ca</h2>
+                <h2>arcade.rate@gmail.com</h2>
                 <h2>Video game data from <a href="#">IDGB Database.</a></h2>
             </div>
 
             <div className='social_media-links'>
-                <InstagramIcon />
-                <FacebookIcon />
-                <MailOutlineIcon />
-                <TwitterIcon />
+                <h2>Connect with us on</h2>
+                <div className='icons'>
+                    <a href="https://www.instagram.com/arcade.rate" target="_blank" rel="noopener noreferrer">
+                        <InstagramIcon />
+                    </a>
+                    <a href="https://www.youtube.com/@ArcadeRate" target="_blank" rel="noopener noreferrer">
+                        <YouTubeIcon />
+                    </a>
+                    <a href="https://x.com/ArcadeRate" target="_blank" rel="noopener noreferrer">
+                        <TwitterIcon />
+                    </a>
+                </div>
             </div>
         </div>
     );
